0fb8ca31a783c4e2c9518ba2d663d23a7fa08756
wuchuang518 <129854950+wuchuang518@users.noreply.github.com>
2023-04-05 14:44:43 +0800

    Update default.ts

--- src/default.ts
+++ src/default.ts
@@ -8,10 +8,8 @@ export const defaultSetting = {
 
 export const defaultMessage = `
 
-- 特感谢 [OpenAI API (gpt-3.5-turbo)](https://platform.openai.com/docs/guides/chat) 和 [Vercel](http://vercel.com/) 提供支持。
+- 特感谢 [OpenAI API (gpt-3.5-turbo)] 和 [Vercel]提供支持。
 
-- 本站由 [尖顶回收](https://mp.weixin.qq.com/s/HTYv9FxUAJDzh0yWLFNotQ) 搭建。
-
-- 想要部署的朋友，可关注公众号：**尖顶回收**，添加我个人微信获取保姆级部署文档，
+- 本站由 [云上情报站] 搭建，关注公众号：**云山情报站**获取最新活动资讯。
 
 - **Shift+Enter** 换行。开头输入 **/** 或者 **空格** 搜索 Prompt 预设。点击输入框滚动到底部。`
