a95ea11d10a43720d9fc918ed793ffd603cc0943
wuchuang518 <129854950+wuchuang518@users.noreply.github.com>
2023-04-05 14:55:06 +0800

    Update default.ts

--- src/default.ts
+++ src/default.ts
@@ -8,8 +8,6 @@ export const defaultSetting = {
 
 export const defaultMessage = `
 
-- 特感谢 [OpenAI API (gpt-3.5-turbo)] 和 [Vercel]提供支持。
-
-- 本站由 [云上情报站] 搭建，关注公众号：**云山情报站**获取最新活动资讯。
+- 本站由 [云上情报站] 搭建，关注公众号：**云山情报站** 获取最新活动资讯。
 
 - **Shift+Enter** 换行。开头输入 **/** 或者 **空格** 搜索 Prompt 预设。点击输入框滚动到底部。`
